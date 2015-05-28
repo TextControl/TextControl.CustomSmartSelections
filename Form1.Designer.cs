045836ab3b1290ef26a3380cc19f331d01beddd0
Gunnar Giffey <gunnar@textcontrol.com>
2015-05-28 17:53:28 +0200

    Added content.

---
+++ Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace tx_sample_selectdecimalnumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textControl1 = new TXTextControl.TextControl();
+            this.SuspendLayout();
+            // 
+            // textControl1
+            // 
+            this.textControl1.ControlChars = true;
+            this.textControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textControl1.Font = new System.Drawing.Font("Arial", 10F);
+            this.textControl1.Location = new System.Drawing.Point(0, 0);
+            this.textControl1.Name = "textControl1";
+            this.textControl1.Size = new System.Drawing.Size(542, 446);
+            this.textControl1.TabIndex = 0;
+            this.textControl1.Text = "Price: 20.33";
+            this.textControl1.ViewMode = TXTextControl.ViewMode.PageView;
+            this.textControl1.DoubleClick += new System.EventHandler(this.textControl1_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 446);
+            this.Controls.Add(this.textControl1);
+            this.Name = "Form1";
+            this.Text = "TX Text Control Sample: Select Decimal Numbers";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TXTextControl.TextControl textControl1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace tx_sample_selectdecimalnumbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textControl1_DoubleClick(object sender, EventArgs e)
+        {
+            string sLineText = String.Empty; // the text of the current line
+            int iLineStartPosition; // the start index of the durrent line
+            int iInputPosition; // the input position in the current line
+
+            // the current line
+            TXTextControl.Line lineCurrent = textControl1.Lines.GetItem(
+                textControl1.InputPosition.TextPosition);
+
+            sLineText = lineCurrent.Text;
+            iLineStartPosition = lineCurrent.Start;
+
+            iInputPosition = textControl1.Selection.Start
+                - iLineStartPosition + 1;
+
+            if (iInputPosition < 0) // nothing selected?
+                return;
+
+            // check the next space backwards
+            int iStart = sLineText.Substring(0, iInputPosition).LastIndexOf(" ");
+            int iLength = 0;
+
+            // check the next space forwards
+            int iNextSpacePosition = 
+                sLineText.Substring(
+                    iInputPosition,
+                    sLineText.Length - iInputPosition).IndexOf(" ");
+
+            // specialties when string is at first or last position in line
+            if (iNextSpacePosition != -1)
+                iLength = iNextSpacePosition + (iInputPosition - iStart) - 1;
+            else
+            {
+                iLength = textControl1.Lines.GetItem(
+                    textControl1.InputPosition.TextPosition).Length - 
+                    iStart - 1;
+            }
+
+            // select the new string
+            textControl1.Select(iLineStartPosition + iStart, iLength);
+        }
+    }
+}
